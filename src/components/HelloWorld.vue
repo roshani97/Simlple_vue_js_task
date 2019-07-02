14b9c74542ee770bebe09c4c568c9ce7dedec402
Roshani Chincholkar <roshanichincholkar97@gmail.com>
2019-07-02 11:48:31 -0700

    add navigation

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,46 @@
 <template>
-<div id="hello">
-<b-button v-b-modal.modal-1 >Add</b-button>
+  <div id="hello">
+  <div id="navi1">
+     <b-navbar type="dark" variant="dark">
+    <b-navbar-nav>
+      <b-nav-text>Welcome</b-nav-text>
+      <b-nav-item href="#">Subject</b-nav-item>
+      <b-nav-item href="#">Topics</b-nav-item>
+      <b-nav-item href="#">Questions</b-nav-item>
+
+      <!-- Navbar dropdowns -->
+      <b-nav-item-dropdown text="Utilities" right>
+        <b-dropdown-item href="#">list1</b-dropdown-item>
+        <b-dropdown-item href="#">list2</b-dropdown-item>
+        <b-dropdown-item href="#">list3</b-dropdown-item>
+        <b-dropdown-item href="#">list</b-dropdown-item>
+      </b-nav-item-dropdown>
+
+      <b-nav-item-dropdown text="Page" right>
+          <b-dropdown-item href="#">p1</b-dropdown-item>
+          <b-dropdown-item href="#">p2</b-dropdown-item>
+          <b-dropdown-item href="#">p3</b-dropdown-item>
+          <b-dropdown-item href="#">p4</b-dropdown-item>
+        </b-nav-item-dropdown>
+
+      <b-nav-item href="#" >Table</b-nav-item>
+      <b-nav-item href="#">Chart</b-nav-item>
+    </b-navbar-nav>
+  </b-navbar>
+  </div>
+  <div id="navi2">
+  <div>
+  <b-navbar type="light" variant="light">
+    <b-nav-form>
+          <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
+          <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+        </b-nav-form>
+  </b-navbar>
+</div>
+  </div>
+
+  <div id="sub_list">
+    <b-button v-b-modal.modal-1 >Add Subject</b-button>
     <b-modal id="modal-1" title="ADD SUBJECT DETAILS" hide-footer>
     <table>
     <tr><td>Subject ID</td><td><input type="text" v-model="sid"></td></tr>
@@ -12,6 +52,7 @@
     </table>
   </b-modal>
 <b-button  @click="deleteTableRow()">Remove</b-button>
+<br>
 <br>
 <table  class="table table-striped">
 <thead>
@@ -39,6 +80,7 @@
 </tr>
 </table>
 </div>
+  </div>
 
 </template>
 
